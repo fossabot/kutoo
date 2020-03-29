ac25ebb7c14cf0be64c5223f86ddd5d74695008d
Federico Morrone <flickmor@gmail.com>
2020-03-29 19:42:17 +0200

    switched yargs to commander

--- src/cli.ts
+++ src/cli.ts
@@ -1,75 +1,52 @@
-import yargs from 'yargs'
-import { isUrl } from './utils'
-import animekit from './index'
-import { resolution, contentType } from './types'
+import { program } from 'commander'
+// import { isUrl } from '@animekit/utils'
+// import animekit from './index'
+// import { resolution, contentType } from '@animekit/types'
+import { version, name } from '../package.json'
 
-const resolutionChoices: readonly resolution[] = ['uhd', 'fhd', 'hd', 'sd', 'low', 'ulow']
-const contentChoices: readonly contentType[] = ['episode', 'season', 'page', 'chapter', 'volume']
+// const resolutionChoices: readonly resolution[] = ['uhd', 'fhd', 'hd', 'sd', 'low', 'ulow']
+// const contentChoices: readonly contentType[] = ['episode', 'season', 'page', 'chapter', 'volume']
 
-const argv = yargs
-  .scriptName('animekit')
-  .usage('$0 url [options]')
-  .wrap(yargs.terminalWidth())
-  .options({
-    resolution: {
-      alias: 'r',
-      description: 'The resolution of the video',
-      choices: resolutionChoices,
-      default: 'fhd'
-    },
-    content: {
-      alias: 'c',
-      description: 'What type of content you\'re trying to download',
-      choices: contentChoices,
-      default: 'episode'
-    },
-    out: {
-      alias: 'o',
-      description: 'Output directory, defaults to the current one',
-      type: 'string',
-      default: './'
-    },
-    dry: {
-      alias: 'd',
-      description: 'Output only info without downloading',
-      type: 'boolean',
-      default: false
-    },
-    pattern: {
-      alias: 'p',
-      description: 'File pattern for downloaded files',
-      type: 'string',
-      default: '<title>/<title>_Ep_<number>.<ext>'
-    }
+program
+  .name(name)
+  .version(version)
+  .option('-r, --resolution', 'The resolution of the video', 'fhd')
+  .option('-c, --content', 'What type of content you\'re trying to download', 'episode')
+  .option('-o, --out', 'Output directory, defaults to the current one', './')
+  .option('-d, --dry', 'Output only info without downloading', false)
+  .option('-p, --pattern', 'File pattern for downloaded files', '<title>/<title>_Ep_<number>.<ext>')
+
+program
+  .command('download <url>', { isDefault: true })
+  .action(url => {
+    console.log(url)
   })
-  .demandCommand(1)
-  .help()
-  .alias('help', 'h')
-  .version()
-  .alias('version', 'v')
-  .detectLocale(false).argv
 
-const urls = argv._
-
-for (const url of urls) {
-  if (isUrl(url)) {
-    doDownload(url, argv)
-      .catch(err => {
-        throw err
-      })
-  } else {
-    console.log(`Error: '${url}' is not a valid url`)
-    process.exit(1)
-  }
+if (process.argv.length < 3) {
+  program.help()
 }
 
-async function doDownload (url: string, argv: any): Promise<void> {
-  console.log('download')
-  await animekit.download(url, argv.out, {
-    filePattern: argv.pattern,
-    resolution: argv.resolution,
-    content: argv.content,
-    subtitles: 'burned',
-    subtitlesLang: 'en'
-  })
-}
+program.parse(process.argv)
+
+// for (const url of urls) {
+//   if (isUrl(url)) {
+//     doDownload(url, argv)
+//       .catch(err => {
+//         throw err
+//       })
+//   } else {
+//     console.log(`Error: '${url}' is not a valid url`)
+//     process.exit(1)
+//   }
+// }
+
+// async function doDownload (url: string, argv: any): Promise<void> {
+//   console.log('download')
+//   await animekit.download(animekit.getInfo(url), argv.out, {
+//     filePattern: argv.pattern,
+//     resolution: argv.resolution,
+//     content: argv.content,
+//     subtitles: 'burned',
+//     subtitlesLang: 'en'
+//   })
+// }
